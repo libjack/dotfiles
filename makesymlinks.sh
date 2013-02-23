4e2bf150379aa8f9755df38c6843f059b63b47ac
John Liberty <libjackjl@gmail.com>
2013-02-23 15:54:45 -0500

    get rid of gitconfig, comment gitignore_global

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="profile bashrc vimrc vim gitconfig gitignore_global"    # list of files/folders to symlink in homedir
+files="profile bashrc vimrc vim gitignore_global"    # list of files/folders to symlink in homedir
 # have to do something about .emacs.d/init.el, etc
 
 ##########
